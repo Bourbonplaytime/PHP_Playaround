da291fc0a5a52cc249f31595472f047c217c1fd9
Bourbs <Bourbonplaytime@gmail.com>
2019-11-25 14:25:55 -0800

    Basic lbs to kg and miles to km program

---
+++ numbers.php
@@ -0,0 +1,18 @@
+<?php
+    //input in pounds
+    $pounds = 47;
+    //conversion factor
+    $lb_to_kg = 0.453592;
+    //calculation
+    $kg = $pounds * $lb_to_kg;
+    //display results
+    echo $pounds . " pounds is equal to " . $kg . " kilograms ";
+    //input in miles
+    $miles = 2.5;
+    //conversion factor
+    $mile_to_km = 1.60934;
+    //calculation
+    $km = $miles * $mile_to_km;
+    //display results
+    echo $miles . " miles is equal to " . $km . " kilometers";
+?>
